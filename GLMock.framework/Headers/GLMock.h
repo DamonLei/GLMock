09805e0158e14635937005b077b3aa3dffe6e097
GL <sh_leig@163.com>
2016-06-15 14:13:05 +0800

    优化URL、double小数位

--- GLMock.framework/Headers/GLMock.h
+++ GLMock.framework/Headers/GLMock.h
@@ -10,14 +10,14 @@
  * 1.导入GLMock.framework和mock.plist文件到项目中
  * 2.在mock.plist中添加自己的《接口名》和《接口协议》格式，如:
  {
-    "接口名称":"定义的接口协议（JSON格式）",
-    "home",{"ad":[{"icon":"http://www.eqiwan.cn","title","推广活动1"}],"recommend":[{"productId":1123,"productName":"推荐产品1","price":1999.99}]}...
+ "接口名称":"定义的接口协议（JSON格式）",
+ "home",{"ad":[{"icon":"http://www.eqiwan.cn","title","推广活动1"}],"recommend":[{"productId":1123,"productName":"推荐产品1","price":1999.99}]}...
  }
  【注意：整型和浮点型设置的数值将会作为Mock随机的范围值，如：productId的Mock数据范围是0-1123，price的Mock数据范围是0-1999.99】
  * 3.使用Mock数据，
-   #import "GLMock.h"
-   NSString *response = [GLMock mockWithApi:@"home"];
-   [GLMock isJsonString:response];    // 查看Mock数据是否是JSON格式
+ #import "GLMock.h"
+ NSString *response = [GLMock mockWithApi:@"home"];
+ [GLMock isJsonString:response];    // 查看Mock数据是否是JSON格式
  *
  ******************************************************/
 
@@ -40,5 +40,12 @@
  *  @return 解析成功/失败
  */
 + (BOOL)isJsonString:(NSString *)jsonString;
+
+/**
+ *  解析Json字符串为数组/字典
+ *
+ *  @param jsonString json字符串
+ */
++ (id)parsingJsonString:(NSString *)jsonString;
 
 @end
